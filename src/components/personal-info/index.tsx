cd57e13fd83d121a49e01afc798422f69265dc26
mertcancet <mertcancet95@gmail.com>
2022-10-08 19:19:02 +0300

    fixed doesnt load image using safari browser

--- src/components/personal-info/index.tsx
+++ src/components/personal-info/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Image from 'next/image';
 
 import { PersolnalInfoContent } from '..';
 import Avatar from '../avatar';
@@ -15,19 +14,19 @@ const PersonalInfo = () => {
       {/* Left top circle */}
       <div className="absolute top-[-50%] left-[-35%] z-0 animate-[moveUp_12s_linear__infinite]">
         <div className="relative w-[300px] h-[300px] z-0 blur-2xl ">
-          <Image src="/images/Rectangle-2.png" layout="fill" alt="" priority />
+          <img src="/ imgs/Rectangle-2.png" alt="" />
         </div>
       </div>
       {/* Right bottom circle */}
       <div className="absolute bottom-[-50%] right-[-35%]  z-0 animate-[moveUp2_15s_linear__infinite] ">
         <div className="relative w-[300px] h-[300px] z-0 blur-2xl  ">
-          <Image src="/images/Rectangle-1.png" layout="fill" alt="" priority />
+          <img src="/ imgs/Rectangle-1.png" alt="" />
         </div>
       </div>
       {/* Yellow circle */}
       <div className="absolute top-[50%] left-[50%]  z-0 animate-[transformYellowCircle_10s_linear__infinite]">
         <div className="relative w-[150px] h-[150px] z-0 blur-2xl ">
-          <Image src="/images/Rectangle-3.png" layout="fill" alt="" priority />
+          <img src="/ imgs/Rectangle-3.png" alt="" />
         </div>
       </div>
     </div>

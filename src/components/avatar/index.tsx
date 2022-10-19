6ff69b42e10aabf32a1e842ec3366c75aacd6e8a
mertcancet <mertcancet95@gmail.com>
2022-10-19 23:13:32 +0300

    add favicon and change z index hologram image

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -11,7 +11,7 @@ const Avatar = () => {
       <div
         className={cx(
           styles.container,
-          'absolute  top-[50px]    grid place-items-center h-[200px] w-[200px]'
+          'absolute  top-[50px] grid place-items-center h-[200px] w-[200px] z-10'
         )}
       />
       <Card className="top-[-50%] left-[50%]  translate-y-[50%] translate-x-[-50%] !rounded-full w-[150px] h-[150px] grid place-items-center absolute  !z-20">

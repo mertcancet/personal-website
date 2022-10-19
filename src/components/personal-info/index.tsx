45e31f2f60dc66d42ac380601874ceb267780266
mertcancet <mertcancet95@gmail.com>
2022-10-20 00:09:17 +0300

    fixed blur issue using safari

--- src/components/personal-info/index.tsx
+++ src/components/personal-info/index.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image';
 import React from 'react';
+import Image from 'next/image';
 
 import { PersolnalInfoContent } from '..';
 import Avatar from '../avatar';
@@ -13,19 +13,19 @@ const PersonalInfo = () => {
         <PersolnalInfoContent />
       </Card>
       {/* Left top circle */}
-      <div className="absolute top-0 left-[-100px] sm:left-[-200px] z-0 animate-[moveUp_12s_linear__infinite]">
+      <div className="absolute top-0 left-[-100px] sm:left-[-200px] z-0 animate-[moveUp_12s_linear__infinite]  blur-2xl ">
         <div className="relative w-[300px] h-[300px] z-0 blur-2xl ">
           <Image src="/images/Rectangle-2.png" layout="fill" alt="" unoptimized priority />
         </div>
       </div>
       {/* Right bottom circle */}
-      <div className="absolute bottom-[-50%] right-[-35%]  z-0 animate-[moveUp2_15s_linear__infinite] ">
+      <div className="absolute bottom-[-50%] right-[-35%]  z-0 animate-[moveUp2_15s_linear__infinite]  blur-2xl">
         <div className="relative w-[300px] h-[300px] z-0 blur-2xl  ">
           <Image src="/images/Rectangle-1.png" layout="fill" alt="" unoptimized priority />
         </div>
       </div>
       {/* Yellow circle */}
-      <div className="absolute top-[50%] left-[50%]  z-0 animate-[transformYellowCircle_10s_linear__infinite]">
+      <div className="absolute top-[50%] left-[50%]  z-0 animate-[transformYellowCircle_10s_linear__infinite]  blur-2xl">
         <div className="relative w-[150px] h-[150px] z-0 blur-2xl ">
           <Image src="/images/Rectangle-3.png" layout="fill" alt="" unoptimized priority />
         </div>
